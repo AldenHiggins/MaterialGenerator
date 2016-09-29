d0f4d9fa50d370a64384f2f368f69854a133d914
Alden Higgins <johnaldenhiggins@gmail.com>
2016-09-29 15:30:47 -0700

    Added support for a material type dropdown and an ismetallic boolean just to further mock up what the end functionality will be

--- Source/MaterialGenerator/Private/MaterialTool.h
+++ Source/MaterialGenerator/Private/MaterialTool.h
@@ -3,6 +3,14 @@
 #include "BaseEditorTool.h"
 #include "MaterialTool.generated.h"
 
+UENUM(BlueprintType)		//"BlueprintType" is essential to include
+enum class EBaseMaterialType : uint8
+{
+	Base_Mat 	UMETA(DisplayName = "Default"),
+	Cloth_Mat 	UMETA(DisplayName = "Cloth"),
+	Metal_Mat	UMETA(DisplayName = "Metal")
+};
+
 UCLASS(Blueprintable)
 class UMaterialTool : public UBaseEditorTool
 {
@@ -16,6 +24,9 @@ public:
 	FString NewMaterialName;
 
 	UPROPERTY(EditAnywhere, Category = "Settings")
+	EBaseMaterialType BaseMaterialType;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
 	UMaterial *BaseMaterial;
 
 	UPROPERTY(EditAnywhere, Category = "Settings")
@@ -26,6 +37,10 @@ public:
 
 	UPROPERTY(EditAnywhere, Category = "Settings")
 	UTexture2D *Specular;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	bool IsMetallic;
+
 
 	UFUNCTION(Exec)
 	void GenerateMaterial();
